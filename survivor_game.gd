2cab720dbf378020231591be948eab2ad8fb0b9b
Kadir Kuruca <kadirkuruca572@gmail.com>
2024-01-31 16:01:47 +0300

    Show game over canvas when player died

--- survivor_game.gd
+++ survivor_game.gd
@@ -6,6 +6,10 @@ func spawn_mob():
 	new_mob.global_position = %PathFollow2D.global_position
 	add_child(new_mob)
 
-
 func _on_timer_timeout():
 	spawn_mob()
+	
+
+func _on_player_health_depleted():
+	%GameOver.visible = true
+	get_tree().paused = true
